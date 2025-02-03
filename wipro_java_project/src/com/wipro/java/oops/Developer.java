5b0bdba36133e3f9502e8b4e0f6634a46ca9e198
ShanmukhaaSai43 <kalugurisai2002@gmail.com>
2025-02-03 12:54:15 +0530

    Day2

--- wipro_java_project/src/com/wipro/java/oops/Developer.java
+++ wipro_java_project/src/com/wipro/java/oops/Developer.java
@@ -14,7 +14,23 @@ public class Developer extends Employee {
 	 * 
 	 */
 	public Developer() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	public static void main(String a[]) {
+		//Child object is instantiated from child constructor
+		//Parent class consumes the properties and behaviors of child class
+		
+		Employee employee = new Developer();
+		
+		//setting the developer details
+		employee.setEmployeeId(7);
+		employee.setEmployeeName("Suhaas");
+		employee.setEmailId("suhaas123@gmail");
+		
+		System.out.println(employee.getEmployeeId());
+		System.out.println(employee.getEmployeeName());
+		System.out.println(employee.getEmailId());
+		
 	}
 
 }
--- wipro_java_project/src/com/wipro/java/oops/Manager.java
+++ wipro_java_project/src/com/wipro/java/oops/Manager.java
@@ -12,5 +12,22 @@ public class Manager extends Employee {
 	public Manager() {
 		
 	}
+	
+	public static void main(String a[]) {
+		//Child object is instantiated from child constructor
+		//Parent class consumes the properties and behaviors of child class
+		
+		Employee employee = new Developer();
+		
+		//setting the manager details
+		employee.setEmployeeId(2);
+		employee.setEmployeeName("Kamlesh");
+		employee.setEmailId("kamlesh2@gmail");
+		
+		System.out.println(employee.getEmployeeId());
+		System.out.println(employee.getEmployeeName());
+		System.out.println(employee.getEmailId());
+		
+	}
 
 }
