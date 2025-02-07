7844113d11d674819bcb721efdcb12c00bc50bf3
ShanmukhaaSai43 <kalugurisai2002@gmail.com>
2025-02-07 12:40:59 +0530

    Day6 Commit-1

---
+++ wipro_java_project/src/com/wipro/java/junit/Calculator.java
@@ -0,0 +1,16 @@
+package com.wipro.java.junit;
+
+public class Calculator {
+
+	//Method to add two numbers
+	public int addition(int num1, int num2) {
+		return num1 + num2;
+		
+	}
+	
+	//Method to subtract two numbers
+	public int subtract(int num1, int num2) {
+		return num1 - num2;
+		
+	}
+}
